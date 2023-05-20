b4fdd22cc00fcb513b52c166ed1a2396240c94f0
dheeerajkashyap <133700803+dheeerajkashyap@users.noreply.github.com>
2023-05-20 16:32:55 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,7 +118,7 @@ function showProjects(projects) {
         <div class="desc">
           <p>${project.desc}</p>
           <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> Know more</a>
         
           </div>
         </div>
