7af2340c7289e92908ea02ea0bebabf0c7460846
dheeerajkashyap <133700803+dheeerajkashyap@users.noreply.github.com>
2025-04-17 23:42:53 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,12 +59,12 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Dheeraj Kashyap";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
-            document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "assets/images/favhand.png");
+            document.title = "INFORMATION SYSTEM";
+            $("#favicon").attr("href", "assets/images/content-management-system.png");
         }
     });
 
