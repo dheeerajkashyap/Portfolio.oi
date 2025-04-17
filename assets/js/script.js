8e47335a3081a3a6551fc555b566bad5daea2543
dheeerajkashyap <133700803+dheeerajkashyap@users.noreply.github.com>
2025-04-17 23:53:15 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["AUTOMATION", "ANALYTICS", "MARKETING", "PRODUCT"],
+    strings: ["ANALYTICS", "TECHNOLOGY", "BUSINESS"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
